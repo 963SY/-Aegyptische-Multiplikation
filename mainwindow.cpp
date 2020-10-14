4272e4d6bda15682fcb8045c4d680084680a5ee7
natoufah-web <67907827+natoufah-web@users.noreply.github.com>
2020-10-14 12:21:33 +0300

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,72 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QPlainTextEdit>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setStyleSheet("{background-image: url(:/images/Egyptian_.png);}");
+    connect(ui->actionAbout, SIGNAL(triggered(bool)), this, SLOT(ueber_clicked_About()));
+    connect(ui->actioninfo, SIGNAL(triggered(bool)), this, SLOT(ueber_clicked_info()));
+
+   // this->setStyleSheet("color: white; background-color: White; selection-color: red;"
+   // "QPushButton#pushButton_clear{ background-color: yellow }");
+    //Qpus->setStyleSheet("QPushButton#pushButton_clear{ background-color: yellow }");
+}
+
+void MainWindow::on_pushButton_Multi_clicked()
+{
+    QString xtext =ui->lineEdit_x->text();
+    double x= xtext.toDouble();
+    QString ytext =ui->lineEdit_y->text();
+    double y=ytext.toDouble();
+    std::vector<int> links, rechts;
+    links.push_back(x);
+    rechts.push_back(y);
+
+    while(links.at(links.size()-1) != 1){
+        links.push_back(links.at(links.size()-1)/2);
+        rechts.push_back(rechts.at(rechts.size()-1)*2);
+    }
+
+    for(int i = 0; i < links.size(); i++){
+        if(links.at(i) % 2 != 0){
+    //  std::cout << links.at(i) << " " << rechts.at(i) << std::endl;
+      //      QString sesultString = "";
+        //     ui->label_Methodeshow->setText(sesultString.setNum(links.at(i)));
+      double  Ergbnis = Ergbnis + rechts.at(i);
+      QString resultString = "";
+    ui->label_MalShow->setText(resultString.setNum(Ergbnis));
+    }
+ }
+}
+
+
+void MainWindow::on_pushButton_clear_clicked()
+{
+     ui->lineEdit_x->setText("");
+     ui->lineEdit_y->setText("");
+     ui->label_MalShow->setText("Danke..");
+}
+
+void MainWindow::ueber_clicked_About()
+{
+    QMessageBox::about(this ,"infos","Ägypter kannten die alten Methode, um schriftlich zu multiplizieren. Sie nutzen dabei die Eigenschaft aus, dass jeder Multiplikator als Summe von 2er Potenzen dargestellt werden kann. \nmore infos: https://de.wikipedia.org/wiki/Mathematik_im_Alten_Ägypten");
+}
+
+void MainWindow::ueber_clicked_info()
+{
+    QMessageBox::about(this ,"About"," infos unter:  \n hello@natouf.de  ");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+
